042994e318eb057e86a1b87c24bbaf649f88f29e
Yevheniia <tobolovskaya@gmail.com>
2025-08-17 13:29:17 +0200

    processor

--- alerts_consumer.py
+++ alerts_consumer.py
@@ -2,29 +2,31 @@ from confluent_kafka import Consumer
 from configs import kafka_config, topics_configurations
 import json
 
-def _conf(kc):
+def consumer_conf(conf, group_id):
     return {
-        "bootstrap.servers": ",".join(kc["bootstrap_servers"]),
-        "security.protocol": kc["security_protocol"],
-        "sasl.mechanism": kc["sasl_mechanism"],
-        "sasl.username": kc["username"],
-        "sasl.password": kc["password"],
-        "group.id": "alerts_reader",
+        "bootstrap.servers": ",".join(conf["bootstrap_servers"]),
+        "security.protocol": conf["security_protocol"],
+        "sasl.mechanism": conf["sasl_mechanism"],
+        "sasl.username": conf["username"],
+        "sasl.password": conf["password"],
+        "group.id": group_id,
         "auto.offset.reset": "earliest",
         "enable.auto.commit": True,
     }
 
-topics = [topics_configurations["topic_temp"]["name"],
-          topics_configurations["topic_hum"]["name"]]
+topics = [
+    topics_configurations["topic_temp"]["name"],
+    topics_configurations["topic_hum"]["name"],
+]
 
-c = Consumer(_conf(kafka_config))
+c = Consumer(consumer_conf(kafka_config, "alerts_reader"))
 c.subscribe(topics)
 print("[alerts] listening:", topics)
 
 try:
     while True:
         msg = c.poll(1.0)
-        if msg is None: 
+        if msg is None:
             continue
         if msg.error():
             print("❌", msg.error())
--- processor.py
+++ processor.py
@@ -2,49 +2,54 @@ from confluent_kafka import Consumer, Producer
 from configs import kafka_config, topics_configurations
 import json
 
-def _base_conf(kc):
+def base_client(conf):
     return {
-        "bootstrap.servers": ",".join(kc["bootstrap_servers"]),
-        "security.protocol": kc["security_protocol"],
-        "sasl.mechanism": kc["sasl_mechanism"],
-        "sasl.username": kc["username"],
-        "sasl.password": kc["password"],
+        "bootstrap.servers": ",".join(conf["bootstrap_servers"]),
+        "security.protocol": conf["security_protocol"],
+        "sasl.mechanism": conf["sasl_mechanism"],
+        "sasl.username": conf["username"],
+        "sasl.password": conf["password"],
     }
 
-src = topics_configurations["topic_sensors"]["name"]
-temp_t = topics_configurations["topic_temp"]["name"]
-hum_t  = topics_configurations["topic_hum"]["name"]
+src_topic = topics_configurations["topic_sensors"]["name"]
+temp_topic = topics_configurations["topic_temp"]["name"]
+hum_topic  = topics_configurations["topic_hum"]["name"]
 
-c = Consumer({**_base_conf(kafka_config),
-              "group.id": "processor_group",
-              "auto.offset.reset": "earliest",
-              "enable.auto.commit": True})
+c = Consumer({
+    **base_client(kafka_config),
+    "group.id": "processor_group",
+    "auto.offset.reset": "earliest",
+    "enable.auto.commit": True,
+})
+p = Producer({**base_client(kafka_config), "acks": "all"})
 
-p = Producer({**_base_conf(kafka_config), "acks": "all"})
-
-c.subscribe([src])
-print(f"[processor] {src} → ({temp_t}, {hum_t})")
+c.subscribe([src_topic])
+print(f"[processor] {src_topic} → ({temp_topic}, {hum_topic})")
 
 try:
     while True:
         msg = c.poll(1.0)
-        if msg is None: 
+        if msg is None:
             continue
         if msg.error():
             print("❌", msg.error())
             continue
+
         data = json.loads(msg.value().decode("utf-8"))
-        temp = data.get("temperature", 0)
-        hum  = data.get("humidity", 0)
-        if temp > 40:
+        temp = data.get("temperature")
+        hum  = data.get("humidity")
+
+        if isinstance(temp, (int, float)) and temp > 40:
             alert = {**data, "type": "TEMP_HIGH", "msg": f"T>{temp}°C > 40"}
-            p.produce(temp_t, json.dumps(alert))
+            p.produce(temp_topic, json.dumps(alert).encode("utf-8"))
             print("TEMP_ALERT:", alert)
-        if hum > 80 or hum < 20:
+
+        if isinstance(hum, (int, float)) and (hum > 80 or hum < 20):
             kind = "HUM_HIGH" if hum > 80 else "HUM_LOW"
             alert = {**data, "type": kind, "msg": f"Humidity {hum}% out of range"}
-            p.produce(hum_t, json.dumps(alert))
+            p.produce(hum_topic, json.dumps(alert).encode("utf-8"))
             print("HUM_ALERT:", alert)
+
         p.poll(0)
 except KeyboardInterrupt:
     pass
